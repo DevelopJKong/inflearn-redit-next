3daea36b8ed4caff8940c698612208c4241dfaa7
cafe small house <ybaram40@gmail.com>
2022-12-30 23:14:33 +0900

    회원 가입 페이지 기능 생성(1) 완료

--- client/src/components/InputGroup.tsx
+++ client/src/components/InputGroup.tsx
@@ -4,60 +4,59 @@ import tw from 'twin.macro';
 import cls from 'classnames';
 
 interface InputGroupProps {
-  className?: string;
-  type?: string;
-  placeholder?: string;
-  value?: string;
-  error?: string | undefined;
-  setValue: (str: string) => void;
+   className?: string;
+   type?: string;
+   placeholder?: string;
+   value?: string;
+   error?: string | undefined;
+   setValue: (str: string) => void;
 }
 
 const Container = styled.div.attrs(({ className }) => {
-  return {
-    className,
-  };
+   return {
+      className,
+   };
 })``;
 
-const Input = styled.div.attrs<{ className?: string; type?: string; style?: object }>(
-  ({ className, type }) => {
-    return {
+const Input = styled.input.attrs<{ className?: string; type?: string; style?: object }>(({ className, type }) => {
+   return {
       type,
       className,
-    };
-  },
-)<{ className?: string; type?: string; style?: object }>``;
+   };
+})<{ className?: string; type?: string; style?: object }>``;
 
 const SmallText = styled.small.attrs(({ className }) => {
-  return {
-    className,
-  };
+   return {
+      className,
+   };
 })``;
 
 const InputGroup: React.FC<InputGroupProps> = ({
-  className = 'mb-2',
-  type = 'text',
-  placeholder = '',
-  error,
-  value,
-  setValue,
+   className = 'mb-2',
+   type = 'text',
+   placeholder = '',
+   error,
+   value,
+   setValue,
 }) => {
-  return (
-    <Container className={className}>
-      <Input
-        type={type}
-        className={cls(
-          `w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
-          {
-            'border-red-500': error,
-          },
-        )}
-        style={{ minWidth: 300 }}
-        placeholder={placeholder}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
-      />
-      <SmallText className='font-medium text-red-500'>{error}</SmallText>
-    </Container>
-  );
+   return (
+      <Container className={className}>
+         <Input
+            type={type}
+            value={value}
+            className={cls(
+               `w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
+               {
+                  'border-red-500': error,
+               },
+            )}
+            style={{ minWidth: 300 }}
+            placeholder={placeholder}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+         />
+         <SmallText className='font-medium text-red-500'>{error}</SmallText>
+      </Container>
+   );
 };
 
 export default InputGroup;
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,50 +1,71 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import InputGroup from '../components/InputGroup';
 
 const Container = styled.div`
-  ${tw`bg-white`}
+   ${tw`bg-white`}
 `;
 
 const ContentWrapper = styled.div`
-  ${tw`flex flex-col items-center justify-center h-screen p-6`}
+   ${tw`flex flex-col items-center justify-center h-screen p-6`}
 `;
 
 const Content = styled.div`
-  ${tw`w-10/12 mx-auto md:w-96`}
+   ${tw`w-10/12 mx-auto md:w-96`}
 `;
 
 const Title = styled.h1`
-  ${tw`mb-2 text-lg font-medium`}
+   ${tw`mb-2 text-lg font-medium`}
 `;
 const Form = styled.form``;
 const Button = styled.button`
-  ${tw`w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded`}
+   ${tw`w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded`}
 `;
 const SmallText = styled.small``;
 
-const LinkTag = styled.a`
-  ${tw`ml-1 text-blue-500 uppercase`}
+const LinkTag = styled(Link)`
+   ${tw`ml-1 text-blue-500 uppercase`}
 `;
 
-const register = () => {
-  return (
-    <Container>
-      <ContentWrapper>
-        <Content>
-          <Title>회원가입</Title>
-          <Form>
-            <Button>회원가입</Button>
-          </Form>
-          <SmallText>
-            이미 가입 하셨나요?
-            <Link href={'/login'}>로그인</Link>
-          </SmallText>
-        </Content>
-      </ContentWrapper>
-    </Container>
-  );
+const Register = () => {
+   const [email, setEmail] = useState('');
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   const [errors, setErrors] = useState<any>({});
+
+   return (
+      <Container>
+         <ContentWrapper>
+            <Content>
+               <Title>회원가입</Title>
+               <Form>
+                  <InputGroup placeholder='Email' type='email' value={email} setValue={setEmail} error={errors.email} />
+                  <InputGroup
+                     placeholder='Username'
+                     type='text'
+                     value={username}
+                     setValue={setUsername}
+                     error={errors.username}
+                  />
+                  <InputGroup
+                     placeholder='Password'
+                     type='password'
+                     value={password}
+                     setValue={setPassword}
+                     error={errors.password}
+                  />
+                  <Button>회원가입</Button>
+               </Form>
+               <SmallText>
+                  이미 가입 하셨나요? &nbsp;
+                  <LinkTag href={'/login'}>로그인&nbsp;&rarr;</LinkTag>
+               </SmallText>
+            </Content>
+         </ContentWrapper>
+      </Container>
+   );
 };
 
-export default register;
+export default Register;
